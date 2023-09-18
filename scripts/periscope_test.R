e9716934c8cd9050b4da3c51e43dd954b906b264
mattchen0421 <chenweining2003@gmail.com>
2023-09-18 10:20:47 +0800

    peroscope

--- scripts/periscope_test.R
+++ scripts/periscope_test.R
@@ -2,7 +2,6 @@ library(shiny)
 library(periscope)
 library(ggplot2)
 ui <- fluidPage(
-    plotOutput("plot"),
     wellPanel(
         downloadablePlotUI(
             "object_id1",
@@ -17,27 +16,37 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     ss_userAction.Log <- periscope:::fw_get_user_log()
-    output$plot <- renderPlot(test())
-    data <- reactive(iris)
-    plotInput <- function(variables) {
-        p <- ggplot(data = data(), aes(Sepal.Length, Sepal.Width)) +
-            geom_point()
-        p
-    }
-    test <- reactive({
-        ggplot(data = data(), aes(Sepal.Length, Sepal.Width)) +
-            geom_point()
-        
+    
+    png <- reactive({
+
+        print(
+            ggplot(data = iris, aes(Sepal.Length, Sepal.Width)) +
+                geom_point()
+        )
+
     })
-      
+    plot <- reactive({
+
+        print(
+            ggplot(data = iris, aes(Sepal.Length, Sepal.Width)) +
+                geom_point()
+        )
+    })
+
     downloadablePlot(
         "object_id1",
          logger = ss_userAction.Log,
          filenameroot = "mydownload1",
-         downloadfxns = list(png = plotInput),
-         visibleplot = plotInput
+         downloadfxns = list(png = png),
+         visibleplot = plot
     )
     
 }
 
 shinyApp(ui = ui, server = server)
+
+app_dir = tempdir()
+create_new_application('mysampleapp', location = app_dir, sampleapp = TRUE)
+runApp(paste(app_dir, 'mysampleapp', sep = .Platform$file.sep))
+
+create_new_application(name = 'mytestapp', location = app_dir)
